fe13581c875a824c92cf1820c0cb71edd91523ae
shalaka.betkekar <shalaka.betkekar@creativecapsule.com>
2024-08-01 15:44:26 +0530

    Client section done: carousel styling added variables updated title fonts updated in typogrpahy

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const carouselElement = document.getElementById('mobClientSlider');
+	let items = document.querySelectorAll('.client-carousel .carousel-item');
+
+	const bootstrapCarousel = new bootstrap.Carousel(carouselElement, {
+		interval: false, // Start with no interval
+	});
+	function adjustCarouselAutoplay() {
+		if (window.innerWidth >= 992) {
+			items.forEach((el) => {
+				const minPerSlide = 7;
+				let next = el.nextElementSibling;
+				for (let i = 1; i < minPerSlide; i++) {
+					if (!next) {
+						next = items[0];
+					}
+					let cloneChild = next.cloneNode(true);
+					el.appendChild(cloneChild.children[0]);
+					next = next.nextElementSibling;
+				}
+			});
+			// Disable autoplay and pause the carousel on desktop
+			bootstrapCarousel.pause();
+		} else {
+			items.forEach((el) => {
+				const minPerSlide = 4;
+				let next = el.nextElementSibling;
+				for (let i = 1; i < minPerSlide; i++) {
+					if (!next) {
+						next = items[0];
+					}
+					let cloneChild = next.cloneNode(true);
+					el.appendChild(cloneChild.children[0]);
+					next = next.nextElementSibling;
+				}
+			});
+			// Enable autoplay on mobile
+			bootstrapCarousel.cycle(); // Start the carousel sliding
+			bootstrapCarousel._config.interval = 5000; // Set interval for mobile screens
+		}
+	}
+	// Run the function on page load
+	adjustCarouselAutoplay();
+
+	// Re-run the function if the window is resized
+	window.addEventListener('resize', adjustCarouselAutoplay);
+});
